dcb556916f55795654fc553c5dc87f63d514a077
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-11-01 23:01:12 +0100

    Update Body.jsx

--- Frontend/src/components/body/Body.jsx
+++ Frontend/src/components/body/Body.jsx
@@ -6,6 +6,8 @@ const Body = () => {
         <div className='container'>
         <h2>Welcome to Coin Pricer</h2>
         <p>Looking to find the latest prices for your favorite cryptocurrencies? <br/>Coin Pricer makes it easy to search and track real-time prices for thousands of crypto tokens. 💰🪙</p>
+        <input placeholder='Crypto Token' type='text'/>
+        <input placeholder='Amount' type='number'/>
       </div>
   )
 }
