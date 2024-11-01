4a99291405d86d15420fc4a8ab52f10e1d728591
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-11-01 22:51:07 +0100

    Create Body.jsx

---
+++ Frontend/src/components/body/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+const Body = () => {
+  return (
+        <div className='container'>
+        <h2>Welcome to Coin Pricer</h2>
+        <p>Looking to find the latest prices for your favorite cryptocurrencies? <br/>Coin Pricer makes it easy to search and track real-time prices for thousands of crypto tokens. 💰🪙</p>
+      </div>
+  )
+}
+
+export default Body
