ce81362edbbb5c8ed0cff3e04b6c1de3e1764781
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-11-01 22:14:02 +0100

    Update main.go

--- Backend/main.go
+++ Backend/main.go
@@ -6,12 +6,15 @@ import(
 	"github.com/gin-gonic/gin"
 	"log"
 	"io"
-	
+
 )
 
 func getPrice(c *gin.Context) {
-	id := c.Query("id")	
-	coingeckoUrl := fmt.Sprintf("https://api.coingecko.com/api/v3/simple/price?ids=%v&vs_currencies=usd", id)
+	id := c.Query("id")
+	amount := c.Query("amount")	
+
+
+	coingeckoUrl := fmt.Sprintf("https://pro-api.coinmarketcap.com/v2/tools/price-conversion?amount=%v&symbol=%v", amount, id)
 
 	// Creating Client
 	client := &http.Client{}
@@ -21,6 +24,42 @@ func getPrice(c *gin.Context) {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create request"})
         return
     }
+
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("X-CMC_PRO_API_KEY", "6785dae0-379a-4ef2-a3ea-998bb9a03372")
+
+	// Send request	
+	resp, err := client.Do(req)
+    if err != nil {
+        log.Println("Error making API call:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data"})
+        return
+    }
+    defer resp.Body.Close()
+
+	// Read the response body
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        log.Println("Error reading response body:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response"})
+        return
+    }
+
+    // Return the response from the external API as JSON
+    c.Header("Content-Type", "application/json")
+    c.String(http.StatusOK, string(body))
+}
+
+func tokensList(c *gin.Context){
+	url := "https://api.coingecko.com/api/v3/simple/supported_vs_currencies"
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println("Error creating request:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create request"})
+        return
+	}
 
 	req.Header.Set("accept", "application/json")
 	req.Header.Set("x-cg-pro-api-key", "CG-BRausksF9ekuD2GJondFLLAm")
@@ -45,10 +84,12 @@ func getPrice(c *gin.Context) {
     // Return the response from the external API as JSON
     c.Header("Content-Type", "application/json")
     c.String(http.StatusOK, string(body))
+
 }
 
 func main() {
 	router := gin.Default()
+	router.GET("/supportedList", tokensList)
 	router.GET("/tokenprice", getPrice)
 	router.Run("localhost:8080")
 }
